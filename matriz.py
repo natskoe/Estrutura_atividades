62d5ed87c56b3a6d430249046dc460396020bdde
natskoe <koehlernathy@gmail.com>
2023-09-01 19:31:47 -0300

    Compreensão de matriz e como imprimir

---
+++ matriz.py
@@ -0,0 +1,12 @@
+matriz = [[56, 7, 8, 9],
+          [8, 1 ,32, 6],
+          [21, 7, 20, 8]]
+
+print(matriz[0])
+
+for linha in matriz:
+#imprime a coluna, com os algarismos empilhados.
+    print(linha[3])
+
+#linha, coluna.
+print(matriz [2],[3])
