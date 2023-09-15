3b414e64bec417490f637d1ba129514d5a21220e
natskoe <koehlernathy@gmail.com>
2023-09-15 19:13:25 -0300

    alteracao repo

---
+++ filhas.py
@@ -0,0 +1,32 @@
+#Implemente um programa principal que utilize a fila através de
+#chamada de funções: empilhar, desempilhar, limpar, mostrar fila,
+#acessar o topo da fila, sair do programa informando “Sair”
+
+#clear() - limpar a fila.
+#isEmpty() - verificar se a fila está vazia.
+#isFull() - verificar se a fila está cheia.
+#enqueue() - inserir o elemento ao final da fila.
+#dequeue() - remover o primeiro elemento da fila.
+#first() - retorna o primeiro elemento da fila - sem removê-lo.
+
+class Filas:
+
+    fila = []
+
+    def limpar_fila():
+        pass
+
+    def verificar_vazia():
+        pass
+
+    def verificar_cheia():
+        pass
+
+    def inserir_elemento():
+        pass
+
+    def remover_elemento():
+        pass
+
+    def retornar_primeiro():
+        pass
